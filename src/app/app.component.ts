40c3b46eae479675ee014694c042aef13511080b
Emilio Martel <emilio_martel@outlook.es>
2025-02-09 20:13:49 +0000

    create graph component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { GraphComponent } from './components/graph/graph.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GraphComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/graph/graph.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  imports: [],
+  templateUrl: './graph.component.html',
+  styleUrl: './graph.component.css'
+})
+export class GraphComponent {
+
+  constructor(){}
+
+}
