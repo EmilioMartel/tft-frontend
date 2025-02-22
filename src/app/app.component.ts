31cb012de09883c73b60e8179b9f574bbce676fd
Emilio Martel <emilio_martel@outlook.es>
2025-02-22 15:28:41 +0000

    change view structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { GraphComponent } from './components/graph/graph.component';
+import { GraphMenuComponent } from './components/graph-menu/graph-menu.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [GraphComponent],
+  imports: [
+    GraphComponent,
+    GraphMenuComponent,
+    MatSidenavModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tft-frontend';
+  showFiller = false
+
 }
